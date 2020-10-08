cc6ff83482f99591f9a373bf3c480296842f006e
srosset81 <srosset81@gmail.com>
2020-10-08 17:22:02 +0200

    Use specific colibris ontology

--- services/ldp.service.js
+++ services/ldp.service.js
@@ -10,6 +10,7 @@ module.exports = {
     containers: [
       'hosting-services',
       'hosting-services-types'
-    ]
+    ],
+    defaultJsonContext: 'https://gist.githubusercontent.com/srosset81/8f709146f2e09ecfbed50255c95999fa/raw/e1e747317826440be92b8353783f2bc263a0eba9/colibris-ontology.json'
   }
 };
