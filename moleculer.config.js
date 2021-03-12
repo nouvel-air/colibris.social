3b2259c2e7f8f647b064691e8b4655e848a115e9
srosset81 <srosset81@gmail.com>
2021-03-12 19:09:53 +0100

    Improve dev env

--- moleculer.config.js
+++ moleculer.config.js
@@ -1,5 +1,17 @@
 const CONFIG = require('./config');
 
+// Use the cacher only if Redis is configured
+const cacher = CONFIG.REDIS_CACHE_URL
+  ? {
+    type: 'Redis',
+    options: {
+      prefix: 'action',
+      ttl: 2592000, // Keep in cache for one month
+      redis: CONFIG.REDIS_CACHE_URL
+    }
+  }
+  : undefined;
+
 module.exports = {
   // You can set all ServiceBroker configurations here
   // See https://moleculer.services/docs/0.14/configuration.html
@@ -10,12 +22,5 @@ module.exports = {
     }
     throw error;
   },
-  // cacher: {
-  //   type: 'Redis',
-  //   options: {
-  //     prefix: 'action',
-  //     ttl: 2592000, // Keep in cache for one month
-  //     redis: CONFIG.REDIS_CACHE_URL
-  //   }
-  // }
+  cacher
 };
