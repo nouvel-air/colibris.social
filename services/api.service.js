d900357b30fead67fbd37d0537ce271d560af194
srosset81 <srosset81@gmail.com>
2021-03-25 12:48:54 +0100

    Fix CAS connect

--- services/api.service.js
+++ services/api.service.js
@@ -1,10 +1,9 @@
 const path = require('path');
 const urlJoin = require('url-join');
 const ApiGatewayService = require('moleculer-web');
-
 const { getContainerRoutes } = require('@semapps/ldp');
 const { CasConnector } = require('@semapps/connector');
-
+const { MIME_TYPES } = require('@semapps/mime-types');
 const CONFIG = require('../config');
 
 module.exports = {
@@ -33,22 +32,56 @@ module.exports = {
       casUrl: CONFIG.CAS_URL,
       privateKeyPath: path.resolve(__dirname, '../jwt/jwtRS256.key'),
       publicKeyPath: path.resolve(__dirname, '../jwt/jwtRS256.key.pub'),
-      selectProfileData: authData => ({
-        slug: authData.displayName,
-        preferredUsername: authData.displayName,
-        name: `${authData.field_first_name[0]} ${authData.field_last_name[0]}`
-      }),
+      selectProfileData: authData => {
+        console.log(authData);
+        return ({
+          nick: authData.displayName,
+          email: authData.mail[0],
+          image: authData.field_avatar[0],
+          address: JSON.parse(authData.field_address[0]),
+          latLng: JSON.parse(authData.field_lat_lon[0]),
+          preferredUsername: authData.displayName,
+          name: authData.field_first_name[0],
+          familyName: authData.field_last_name[0]
+        })
+      },
       findOrCreateProfile: async profileData => {
-        let actor = await this.broker.call('activitypub.actor.get', {
-          id: profileData.slug
+        let webId = await this.broker.call('webid.findByEmail', {
+          email: profileData.email
         });
-        if (!actor) {
-          actor = await this.broker.call('activitypub.actor.create', {
-            '@type': 'Person',
-            ...profileData
+
+        if (!webId) {
+          webId = await this.broker.call('webid.create', profileData);
+
+          // Adds PAIR data
+          await this.broker.call('ldp.resource.patch', {
+            resource: {
+              '@context': urlJoin(CONFIG.HOME_URL, 'context.json'),
+              '@id': webId,
+              '@type': ['pair:Person', 'foaf:Person', 'Person'],
+              'pair:firstName': profileData.name,
+              'pair:lastName': profileData.familyName,
+              'pair:e-mail': profileData.email,
+              'pair:image': profileData.image,
+              'pair:hasLocation': {
+                '@type': 'pair:Place',
+                'pair:hasPostalAddress': {
+                  type: 'pair:PostalAddress',
+                  'pair:addressCountry': profileData.address.country_code === 'FR' ? 'France' : profileData.address.country_code,
+                  'pair:addressLocality': profileData.address.locality,
+                  // 'pair:addressStreet': profileData.address.address_line1,
+                  'pair:addressZipCode': profileData.address.postal_code
+                },
+                'pair:label': profileData.address.locality,
+                'pair:latitude': profileData.latLng.lat,
+                'pair:longitude': profileData.latLng.lon
+              }
+            },
+            contentType: MIME_TYPES.JSON
           });
         }
-        return actor.id;
+
+        return webId;
       }
     });
 
