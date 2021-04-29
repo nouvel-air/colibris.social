b70ddc39348472ad4711b220d593a7e1adac9a3b
srosset81 <srosset81@gmail.com>
2021-04-29 15:15:45 +0200

    Mailer to write to users

--- config.js
+++ config.js
@@ -5,13 +5,25 @@ require('dotenv-flow').config();
 module.exports = {
   HOME_URL: process.env.SEMAPPS_HOME_URL,
   DEFAULT_JSON_CONTEXT: process.env.SEMAPPS_DEFAULT_JSON_CONTEXT,
+  // Triple store
   SPARQL_ENDPOINT: process.env.SEMAPPS_SPARQL_ENDPOINT,
   MAIN_DATASET: process.env.SEMAPPS_MAIN_DATASET,
   JENA_USER: process.env.SEMAPPS_JENA_USER,
   JENA_PASSWORD: process.env.SEMAPPS_JENA_PASSWORD,
+  // Connector
   CAS_URL: process.env.SEMAPPS_CAS_URL,
+  // Cache
   QUEUE_SERVICE_URL: process.env.SEMAPPS_QUEUE_SERVICE_URL,
   REDIS_CACHE_URL: process.env.SEMAPPS_REDIS_CACHE_URL,
+  // Email
+  FROM_EMAIL: process.env.SEMAPPS_FROM_EMAIL,
+  FROM_NAME: process.env.SEMAPPS_FROM_NAME,
+  SMTP_HOST: process.env.SEMAPPS_SMTP_HOST,
+  SMTP_PORT: parseInt(process.env.SEMAPPS_SMTP_PORT, 10),
+  SMTP_SECURE: process.env.SEMAPPS_SMTP_SECURE === 'true',
+  SMTP_USER: process.env.SEMAPPS_SMTP_USER,
+  SMTP_PASS: process.env.SEMAPPS_SMTP_PASS,
+  // Tracking
   SENTRY_DSN: process.env.SEMAPPS_SENTRY_DSN,
   SENTRY_ENVIRONMENT: process.env.SEMAPPS_SENTRY_ENVIRONMENT
 };
---
+++ services/mailer.service.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const MailerService = require('moleculer-mail');
+const { MIME_TYPES } = require('@semapps/mime-types');
+const CONFIG = require('../config');
+
+module.exports = {
+  name: 'mailer',
+  mixins: [MailerService],
+  settings: {
+    from: `${CONFIG.FROM_NAME} <${CONFIG.FROM_EMAIL}>`,
+    transport: {
+      host: CONFIG.SMTP_HOST,
+      port: CONFIG.SMTP_PORT,
+      secure: CONFIG.SMTP_SECURE,
+      auth: {
+        user: CONFIG.SMTP_USER,
+        pass: CONFIG.SMTP_PASS,
+      },
+    },
+    templateFolder: path.join(__dirname, "../templates"),
+  },
+  dependencies: ['ldp'],
+  actions: {
+    async contactUser(ctx) {
+      const { userUri, name, email, title, content, emailPredicate, homeUrl, logoUrl } = ctx.params;
+
+      if( !userUri || !emailPredicate || !homeUrl || !logoUrl ) {
+        throw new Error('One or more parameters are missing');
+      }
+
+      const user = await ctx.call('ldp.resource.get', {
+        resourceUri: userUri,
+        accept: MIME_TYPES.JSON
+      });
+
+      await ctx.call('mailer.send', {
+        to: user[emailPredicate],
+        replyTo: `${name} <${email}>`,
+        subject: title,
+        template: 'contact-user',
+        data: {
+          name,
+          email,
+          title,
+          content,
+          contentWithBr: content.replace(/\r\n|\r|\n/g, '<br />'),
+          homeUrl,
+          logoUrl
+        }
+      });
+    },
+    getApiRoutes() {
+      return [
+        {
+          bodyParsers: { json: true },
+          aliases: {
+            [`POST _mailer/contact-user`]: 'mailer.contactUser',
+          }
+        }
+      ];
+    }
+  }
+};
