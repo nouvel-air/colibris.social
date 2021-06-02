78868036e371bc3a2e0c4eb3918a2f0311b01cc3
srosset81 <srosset81@gmail.com>
2021-06-02 12:31:57 +0200

    Upgrade moleculer-web package

--- moleculer.config.js
+++ moleculer.config.js
@@ -15,8 +15,8 @@ const cacher = CONFIG.REDIS_CACHE_URL
 module.exports = {
   // You can set all ServiceBroker configurations here
   // See https://moleculer.services/docs/0.14/configuration.html
-  errorHandler(error, { ctx = {}, event, action }) {
-    if( ctx ) {
+  errorHandler(error, { ctx, event, action }) {
+    if( ctx && ctx.call ) {
       const { requestID, params } = ctx;
       ctx.call('sentry.sendError', { error, requestID, params, event, action });
     }
