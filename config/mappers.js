7da737f2dad814d23a9c9bc4e2b3b3440cc6474d
srosset81 <srosset81@gmail.com>
2022-04-05 11:21:12 +0200

    Improve mappers

--- config/mappers.js
+++ config/mappers.js
@@ -18,10 +18,10 @@ module.exports = [
     mapping: {
       key: 'article',
       category: "NOUVEL ARTICLE DU MAG",
-      title: '{{activity.object.object.pair:label}}',
-      description: '{{activity.object.object.pair:comment}}',
+      title: '{{{activity.object.object.pair:label}}}',
+      description: '{{{activity.object.object.pair:comment}}}',
       image: '{{activity.object.object.pair:depictedBy}}',
-      actionName: 'En savoir plus',
+      actionName: "Voir l'article",
       actionLink: '{{activity.object.object.pair:webPage}}',
     }
   },
@@ -39,10 +39,10 @@ module.exports = [
     mapping: {
       key: 'course',
       category: "NOUVELLES FORMATIONS DE L'UNIVERSITE",
-      title: '{{activity.object.object.pair:label}}',
-      description: '{{activity.object.object.pair:comment}}',
+      title: '{{{activity.object.object.pair:label}}}',
+      description: '{{{activity.object.object.pair:comment}}}',
       image: '{{activity.object.object.pair:depictedBy}}',
-      actionName: 'En savoir plus',
+      actionName: 'Voir la formation',
       actionLink: '{{activity.object.object.pair:webPage}}',
     },
     priority: 2 // Match before the other events
@@ -52,11 +52,13 @@ module.exports = [
     mapping: {
       key: 'event',
       category: "NOUVEAUX EVENEMENTS",
-      title: '{{activity.object.object.pair:label}}',
-      description: '{{activity.object.object.pair:comment}}',
+      title: '{{{activity.object.object.pair:label}}}',
+      description: '{{{activity.object.object.pair:comment}}}',
       image: '{{activity.object.object.pair:depictedBy}}',
-      actionName: 'En savoir plus',
+      actionName: "Voir l'événement",
       actionLink: '{{activity.object.object.pair:webPage}}',
+      latitude: '{{activity.object.object.pair:hasLocation.pair:latitude}}',
+      longitude: '{{activity.object.object.pair:hasLocation.pair:longitude}}',
     }
   },
   {
@@ -64,23 +66,38 @@ module.exports = [
     mapping: {
       key: 'local-group',
       category: "NOUVEAU GROUPE LOCAL",
-      title: '{{activity.object.object.pair:label}}',
-      description: '{{activity.object.object.pair:comment}}',
+      title: '{{{activity.object.object.pair:label}}}',
+      description: '{{{activity.object.object.pair:comment}}}',
       image: '{{activity.object.object.pair:depictedBy}}',
-      actionName: 'En savoir plus',
+      actionName: 'Voir le groupe local',
       actionLink: '{{activity.object.object.pair:webPage}}',
+      latitude: '{{activity.object.object.pair:hasLocation.pair:latitude}}',
+      longitude: '{{activity.object.object.pair:hasLocation.pair:longitude}}',
     }
   },
   {
-    match: announceCreate('pair:Resource'),
+    match: {
+      type: ACTIVITY_TYPES.ANNOUNCE,
+      object: {
+        type: ACTIVITY_TYPES.CREATE,
+        object: {
+          type: 'pair:Resource',
+          'pair:neededBy': {
+            type: 'pair:Project'
+          }
+        }
+      }
+    },
     mapping: {
       key: 'need',
       category: "NOUVEAU BESOIN",
-      title: '{{activity.object.object.pair:label}}',
-      description: '{{activity.object.object.pair:comment}}',
-      image: '{{activity.object.object.pair:depictedBy}}',
-      actionName: 'En savoir plus',
-      actionLink: '{{activity.object.object.pair:webPage}}',
+      title: '{{{activity.object.object.pair:label}}}',
+      description: '{{{activity.object.object.pair:description}}}',
+      image: '{{activity.object.object.pair:neededBy.pair:depictedBy}}',
+      actionName: 'Voir le projet',
+      actionLink: '{{activity.object.object.pair:neededBy.pair:aboutPage}}',
+      latitude: '{{activity.object.object.pair:neededBy.pair:hasLocation.pair:latitude}}',
+      longitude: '{{activity.object.object.pair:neededBy.pair:hasLocation.pair:longitude}}',
     }
   },
   {
@@ -88,11 +105,13 @@ module.exports = [
     mapping: {
       key: 'place',
       category: "NOUVEL ACTEUR PRES DE CHEZ NOUS",
-      title: '{{activity.object.object.pair:label}}',
-      description: '{{activity.object.object.pair:comment}}',
-      image: '{{activity.object.object.pair:depictedBy}}',
-      actionName: 'En savoir plus',
-      actionLink: '{{activity.object.object.pair:webPage}}',
+      title: '{{{activity.object.object.pair:label}}}',
+      description: '{{{activity.object.object.pair:description}}}',
+      image: '{{#if activity.object.object.pair:depictedBy}}{{activity.object.object.pair:depictedBy}}{{else}}https://dev.colibris.social/images/places.png{{/if}}',
+      actionName: "Voir l'acteur",
+      actionLink: '{{activity.object.object.pair:aboutPage}}',
+      latitude: '{{activity.object.object.pair:hasLocation.pair:latitude}}',
+      longitude: '{{activity.object.object.pair:hasLocation.pair:longitude}}',
     }
   },
   {
@@ -109,10 +128,10 @@ module.exports = [
     mapping: {
       key: 'product',
       category: "NOUVEAU PRODUIT LA BOUTIQUE",
-      title: '{{activity.object.object.pair:label}}',
-      description: '{{activity.object.object.pair:description}}',
+      title: '{{{activity.object.object.pair:label}}}',
+      description: '{{{activity.object.object.pair:description}}}',
       image: '{{activity.object.object.pair:depictedBy}}',
-      actionName: 'En savoir plus',
+      actionName: 'Voir le produit',
       actionLink: '{{activity.object.object.pair:aboutPage}}',
     },
     priority: 2 // Match before needs
@@ -122,11 +141,13 @@ module.exports = [
     mapping: {
       key: 'project',
       category: "NOUVEAU PROJET LA FABRIQUE",
-      title: '{{activity.object.object.pair:label}}',
-      description: '{{activity.object.object.pair:comment}}',
+      title: '{{{activity.object.object.pair:label}}}',
+      description: '{{{activity.object.object.pair:comment}}}',
       image: '{{activity.object.object.pair:depictedBy}}',
-      actionName: 'En savoir plus',
+      actionName: 'Voir le projet',
       actionLink: '{{activity.object.object.pair:webPage}}',
+      latitude: '{{activity.object.object.pair:hasLocation.pair:latitude}}',
+      longitude: '{{activity.object.object.pair:hasLocation.pair:longitude}}',
     }
   },
 ]
