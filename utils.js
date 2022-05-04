453906be4566dfc769ae891b354095cf910781d2
srosset81 <srosset81@gmail.com>
2022-05-04 15:04:56 +0200

    Adapt digest title based on frequency

--- utils.js
+++ utils.js
@@ -33,6 +33,14 @@ const getDepartmentName = zip => {
 
 const slugify = label => createSlug(label.trim(), { lang: 'fr', custom: { '.': '.', 'Ǧ': 'g' } });
 
+// Taken from https://stackoverflow.com/a/21623206/7900695
+const distanceBetweenPoints = (lat1, lon1, lat2, lon2) => {
+  const p = 0.017453292519943295; // Math.PI / 180
+  const c = Math.cos;
+  const a = 0.5 - c((lat2 - lat1) * p) / 2 + (c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p))) / 2;
+  return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+};
+
 module.exports = {
   capitalize,
   convertWikiNames,
@@ -40,5 +48,6 @@ module.exports = {
   convertGogoDate,
   getSlugFromUri,
   getDepartmentName,
-  slugify
+  slugify,
+  distanceBetweenPoints
 };
