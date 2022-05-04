3c8e1c92209aa43417f07b8783bf4d8db4a24476
srosset81 <srosset81@gmail.com>
2022-05-04 16:29:23 +0200

    Truncate descriptions

--- services/mailer/activity-mapping.service.js
+++ services/mailer/activity-mapping.service.js
@@ -1,9 +1,15 @@
+const truncate = require('lodash.truncate');
 const { ActivityMappingService } = require('@semapps/activitypub');
 const mappers = require('../../config/mappers');
 
 module.exports = {
   mixins: [ActivityMappingService],
   settings: {
-    mappers
+    mappers,
+    handlebars: {
+      helpers: {
+        truncate: (length, text) => truncate(text, { length, separator: ' ' })
+      }
+    }
   }
 };
