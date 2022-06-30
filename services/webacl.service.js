30a8f4c38c0917e9e594605d89df37b3555ab40d
srosset81 <srosset81@gmail.com>
2022-06-30 18:03:35 +0200

    Add Florian and Jérémy as admins

--- services/webacl.service.js
+++ services/webacl.service.js
@@ -7,7 +7,9 @@ module.exports = {
   settings: {
     baseUrl: CONFIG.HOME_URL,
     superAdmins: [
-      urlJoin(CONFIG.HOME_URL, 'users', '11cabcfa-0293-4e71-88f5-84c13978b1ca')
+      urlJoin(CONFIG.HOME_URL, 'users', '11cabcfa-0293-4e71-88f5-84c13978b1ca'), // Sébastien
+      urlJoin(CONFIG.HOME_URL, 'users', '7d49156f-0177-4def-a2ac-e650bedec457'), // Florian
+      urlJoin(CONFIG.HOME_URL, 'users', 'jeremy') // Jérémy
     ]
   }
 };
